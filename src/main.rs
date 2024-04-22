fad054dacb21927cd7cf843e06ffac4268d04641
Dedy Styawan <dedystyawan@Dedys-MacBook-Pro.local>
2024-04-22 11:30:30 +0700

    prod mode

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,11 @@
-pub type Result<T> = core::result::Result<T, Error>;
-pub type Error = Box<dyn std::error::Error>;
-
+mod error;
 mod fs;
+
+pub use self::error::{Error, Result};
 use crate::fs::list_files;
 
 fn main() -> Result<()> {
     let files = list_files(".")?;
     println!("{files:#?}");
-
     Ok(())
 }
-
-// fn list_files(path: &str) -> Result<Vec<String>> {
-//     let files: Vec<String> = std::fs::read_dir(path)?
-//         // let files: Vec<String> = std::fs::read_dir(path)
-//         // .map_err(|err| format!("error while reading dir: cause {err}"))?
-//         .filter_map(|re| re.ok())
-//         .filter(|e| e.file_type().map(|ft| ft.is_file()).unwrap_or(false))
-//         .filter_map(|e| e.file_name().into_string().ok())
-//         .collect();
-//     if files.is_empty() {
-//         return Err("Cannot list empty folder".into());
-//     }
-//     Ok(files)
-// }
